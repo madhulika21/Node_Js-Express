4957827ceadd902c6f6dc1e5091da3e678eacd25
madhulika21 <madhulikarichhariya@gmail.com>
2019-04-12 11:31:30 +0530

    Express Serve Static Files

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next) => {
 console.log(req.header);
 res.statusCode=200;
